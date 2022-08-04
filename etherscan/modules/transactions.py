f08529419f7ea7539a1e3444545c0ffaa8ed69c1
EmperorMew <42871398+EmperorMew@users.noreply.github.com>
2022-08-04 11:26:26 -0500

    reverted

--- etherscan/modules/transactions.py
+++ etherscan/modules/transactions.py
@@ -4,6 +4,17 @@ from etherscan.enums.modules_enum import ModulesEnum as modules
 
 
 class Transactions:
+    @staticmethod
+    def get_contract_execution_status(txhash: str) -> str:
+        url = (
+            f"{fields.MODULE}"
+            f"{modules.TRANSACTION}"
+            f"{fields.ACTION}"
+            f"{actions.GET_STATUS}"
+            f"{fields.TXHASH}"
+            f"{txhash}"
+        )
+        return url
 
     @staticmethod
     def get_tx_receipt_status(txhash: str) -> str:
