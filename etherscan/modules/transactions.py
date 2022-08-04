6cca36e47bedd515879509627c0980569346a886
EmperorMew <42871398+EmperorMew@users.noreply.github.com>
2022-08-04 11:00:33 -0500

    cleaned up unused calls

--- etherscan/modules/transactions.py
+++ etherscan/modules/transactions.py
@@ -4,17 +4,6 @@ from etherscan.enums.modules_enum import ModulesEnum as modules
 
 
 class Transactions:
-    @staticmethod
-    def get_contract_execution_status(txhash: str) -> str:
-        url = (
-            f"{fields.MODULE}"
-            f"{modules.TRANSACTION}"
-            f"{fields.ACTION}"
-            f"{actions.GET_STATUS}"
-            f"{fields.TXHASH}"
-            f"{txhash}"
-        )
-        return url
 
     @staticmethod
     def get_tx_receipt_status(txhash: str) -> str:
